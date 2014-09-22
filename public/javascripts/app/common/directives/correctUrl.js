b18f5dad3a46465e0455d738ce489bea3e561523
Alexander Kochergin <ejiqpep@gmail.com>
2014-09-22 08:04:59 +0300

    Added youtube validation

--- public/javascripts/app/common/directives/correctUrl.js
+++ public/javascripts/app/common/directives/correctUrl.js
@@ -9,8 +9,14 @@
             require: 'ngModel',
             link: function (scope, elem, attrs, ngModel) {
 
+                function ytVidId(url) {
+                    var p = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+                    if (_.isUndefined(url)) return false;
+                    return (url.match(p)) ? RegExp.$1 : false;
+                }
+
                 ngModel.$parsers.unshift(function(value) {
-                    if (value == 'youtube') {
+                    if (ytVidId(value)) {
                         ngModel.$setValidity('validUrl', true);
                         return true;
                     }
@@ -21,7 +27,7 @@
                 });
 
                 ngModel.$formatters.unshift(function(value) {
-                    if (value == 'youtube') {
+                    if (ytVidId(value)) {
                         ngModel.$setValidity('validUrl', true);
                         return true;
                     }
--- public/javascripts/app/modules/search/SearchCtrl.js
+++ public/javascripts/app/modules/search/SearchCtrl.js
@@ -4,19 +4,28 @@
     var SearchCtrl;
     SearchCtrl = function (PlaylistFactory) {
         var _this = this;
+        window.fff = this;
 
         _this.PlaylistFactory = PlaylistFactory;
     };
 
-    SearchCtrl.prototype.submit = function () {
+    SearchCtrl.prototype.submit = function (form) {
         var _this = this;
-        debugger
-        console.log ('form', _this.form);
+
+        if (form.$invalid) return;
 
         console.log('submit', _this.search);
         _this.PlaylistFactory.items.push(_this.search);
         _this.search = null;
+        form.$setPristine(true);
 
+    };
+
+    SearchCtrl.prototype.showError = function (form) {
+        if (form.url.$error.validUrl && form.$dirty)
+            return true;
+        else
+            return false;
     };
 
     SearchCtrl.$inject = ['PlaylistFactory'];
